e76a5e58b8aa47e8a20b3a6b57f8f6b6a6e34320
eshwarprasadv5 <83694502+eshwarprasadv5@users.noreply.github.com>
2022-11-17 12:53:35 +0530

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,11 +1,7 @@
 from openpyxl import Workbook, load_workbook
 import sys
-input_file_name = sys.argv[1]
-reset_values=sys.argv[3]
 wb = load_workbook('/var/lib/jenkins/workspace/Automation/Testsuite.xlsx')
 sh=wb['TestSuite'] 
-wb2=load_workbook(input_file_name)
-sh2 = wb2['Testcases']
 test_folders_loc=[]
 for row in sh['B']:
         test_folders_loc.append(row.value)
@@ -14,23 +10,14 @@ test_suite_val1=[]
 for col in sh['A']:
         test_suite_val1.append(col.value)
 print(test_suite_val1)
-if reset_values=="True":
-    current_row=2
-    for eachRow in sh.iter_rows():
-        sh.cell(row=current_row,column=1).value=0
-        current_row+=1
-    wb.save('/var/lib/jenkins/workspace/Automation/Testsuite.xlsx')
-    current_row2=2
-    for eachRow2 in sh2.iter_rows():
-        sh2.cell(row=current_row2,column=1).value=0
-        current_row2+=1
-    wb2.save(input_file_name)
-else: exit
 if(input_file_name in test_folders_loc):
     t1=test_folders_loc.index(input_file_name)+1
     sh['A'+str(t1)].value=1
     print(sh['A'+str(t1)].value)
     wb.save('/var/lib/jenkins/workspace/Automation/Testsuite.xlsx')
+        wb2=load_workbook(input_file_name)
+sh2 = wb2['Testcases']
+input_file_name = sys.argv[1]
 if input_file_name=='/var/lib/jenkins/workspace/Automation/person_dim_test_suite.xlsx':
    input_product_name = sys.argv[2]
    if input_product_name=='telecom-dev':
